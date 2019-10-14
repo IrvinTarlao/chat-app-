c7021e080e41807101a81f491a84183a92e85848
irvintarlao <irvin.tarlao@gmail.com>
2019-10-14 11:21:42 +0200

    setState online

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,24 +1,37 @@
 import React from 'react';
 import './Contact.css';
 
-
-
-function Contact(props) {
-    return (
-        <div className="Contact">
-            <img className="avatar" src={props.avatar} alt="avatar"/>
-            <div>
-                <div className="name">{props.name}</div>
-
-                <div className="status">
+class Contact extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            online: props.online,
+        }
+    }
+    render() {
+        return (
+            <div className="Contact">
+                <img className="avatar" src={this.props.avatar} alt="avatar"/>
+                <div>
+                    <div className="name">{this.props.name}</div>
+    
+                    <div className="status">
+                    
+                        <span
+                        onClick={event => {
+                            const newOnline = !this.state.online;
+                            this.setState({ online: newOnline });}}
                 
-                    <span className={props.online ? "status-online" : "status-offline"} />
-                    <span className="status-text">{props.online ? "Online" : "Offline"}</span>
-
+                        className={this.state.online ? "status-online" : "status-offline"} />
+                        <span className="status-text">{this.state.online ? "Online" : "Offline"}</span>
+    
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
+
+
 
 export default Contact;
